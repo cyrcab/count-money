ebb4b1f392a729ac29905526597d9ddbc03612c0
ClementMendes <clement.mendes@epitech.eu>
2023-11-27 16:09:15 +0100

    :construction: début d'une infinite loop d'affichage

--- packages/front-end/src/Components/HeadBandCryptoPrice.tsx
+++ packages/front-end/src/Components/HeadBandCryptoPrice.tsx
@@ -1,10 +1,52 @@
-import "../Css/HeadBandCryptoPrice.css";
+import React from 'react';
 import { Container } from "@mui/material";
+import "../Css/HeadBandCryptoPrice.css";
 
 const HeadBandCryptoPrice: React.FC = () => {
   return (
-    <Container className="containerHeadBand" maxWidth='xl' >
-      <h1 style={{ color: "#FEDA84" }}>Bandeau scrollable</h1>
+    <Container className="containerHeadBand" maxWidth="xl">
+      <div className="container">
+        <div className="horizontal-scrolling-items">
+          <div className="horizontal-scrolling-items__item">
+            Bitcoin: $60,000
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Ethereum: $2,000
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Cardano: $1.50
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Binance Coin: $300
+          </div>
+          {/* Dupliquez les éléments pour assurer un défilement continu */}
+          <div className="horizontal-scrolling-items__item">
+            Bitcoin: $60,000
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Ethereum: $2,000
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Cardano: $1.50
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Binance Coin: $300
+          </div>
+          {/* Dupliquez les éléments à nouveau pour une transition plus fluide */}
+          <div className="horizontal-scrolling-items__item">
+            Bitcoin: $60,000
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Ethereum: $2,000
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Cardano: $1.50
+          </div>
+          <div className="horizontal-scrolling-items__item">
+            Binance Coin: $300
+          </div>
+        </div>
+      </div>
     </Container>
   );
 };
