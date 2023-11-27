c21ac3d972425f76a7002b50e504a0c67ac37bb8
AlexMerigot <104435310+AlexMerigot@users.noreply.github.com>
2023-11-27 20:23:28 +0100

    Carousel OK mais problème Data Flux RSS CORS

---
+++ packages/front-end/src/Pages/CarouselRssFlux/CarouselRssFlux.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import img1 from "../../assets/s1222i7.jpg";
+import "../../Css/CarouselRssFlux.css";
+import raccoon from "../../assets/raccoon.jpg";
+
+const CarouselRssFlux: React.FC = () => {
+    
+    const [tests] = React.useState([
+        { image: img1, description: "count-moneyackagesront-end> " },
+        { image: raccoon, description: "}>Liste des Cryptomonnaies" },
+        { image: img1, description: "Description3" },
+        { image: raccoon, description: "rial/Butto" },
+        { image: img1, description: "s Cryptomonn" },
+        { image: raccoon, description: "Description6" },
+        { image: img1, description: "Description7" },
+        { image: raccoon, description: "Description8" },
+        { image: img1, description: "Description9" },
+        { image: raccoon, description: "Description10" },
+      ]);
+
+    const groupedImages = [];
+    while (tests.length > 0) {
+      groupedImages.push(tests.splice(0, 5));
+    }
+         
+    return (
+        <Carousel className="Showcase" animation="slide" autoPlay={true} swipe={false} interval={10000} duration={400} indicators={false} navButtonsAlwaysVisible={true} NextIcon="" PrevIcon="" >
+          {groupedImages.map((imagesGroup, index) => (
+        <div className="Showcase_div" key={index}>
+          {imagesGroup.map(({ image, description }, subIndex) => (
+            <div key={index * 4 + subIndex}>
+              <img className="Showcase_img" src={image} alt={`img-${index * 4 + subIndex}`} />
+              <div className="Showcase_description">{description}</div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+    
+    export default CarouselRssFlux;
--- packages/front-end/src/Pages/Home.tsx
+++ packages/front-end/src/Pages/Home.tsx
@@ -1,15 +1,19 @@
 // Home.tsx
 import Button from "@mui/material/Button";
 import "../Css/Home.css";
+import CarouselRss from "./CarouselRssFlux/CarouselRssFlux";
 
 const Home: React.FC = () => {
+
   return (
     <div className="containerHome">
       <h1 style={{ color: "#FEDA84" }}>Liste des Cryptomonnaies</h1>
       <Button variant="contained" color="primary">
         Salut
       </Button>
+      <CarouselRss />
     </div>
+
   );
 };
 
