80ff21368b36f885785265ea867931f82cf2e117
ClementMendes <clement.mendes@epitech.eu>
2023-11-27 17:05:26 +0100

    :tada: infinite loop with brut string

--- packages/front-end/src/Components/HeadBandCryptoPrice.tsx
+++ packages/front-end/src/Components/HeadBandCryptoPrice.tsx
@@ -1,50 +1,82 @@
-import React from 'react';
+import React from "react";
 import { Container } from "@mui/material";
 import "../Css/HeadBandCryptoPrice.css";
+
+const data = [
+  "Bitcoin: $60,000",
+  "Ethereum: $2,000",
+  "Cardano: $1.50",
+  "Binance Coin: $300",
+  "ripple: $0.50",
+  "Dogecoin: $0.10",
+  "Tether: $1.00",
+  "Polkadot: $40.00",
+  "Bitcoin Cash: $500",
+];
 
 const HeadBandCryptoPrice: React.FC = () => {
   return (
     <Container className="containerHeadBand" maxWidth="xl">
-      <div className="container">
-        <div className="horizontal-scrolling-items">
-          <div className="horizontal-scrolling-items__item">
-            Bitcoin: $60,000
+      <div className="slider">
+        <div className="slide-track">
+          
+          <div className="slide">
+            <h1>{data[0]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Ethereum: $2,000
+          <div className="slide">
+            <h1>{data[1]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Cardano: $1.50
+          <div className="slide">
+            <h1>{data[2]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Binance Coin: $300
+          <div className="slide">
+            <h1>{data[3]}</h1>
           </div>
-          {/* Dupliquez les éléments pour assurer un défilement continu */}
-          <div className="horizontal-scrolling-items__item">
-            Bitcoin: $60,000
+          <div className="slide">
+            <h1>{data[4]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Ethereum: $2,000
+          <div className="slide">
+            <h1>{data[5]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Cardano: $1.50
+          <div className="slide">
+            <h1>{data[6]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Binance Coin: $300
+          <div className="slide">
+            <h1>{data[7]}</h1>
           </div>
-          {/* Dupliquez les éléments à nouveau pour une transition plus fluide */}
-          <div className="horizontal-scrolling-items__item">
-            Bitcoin: $60,000
+          <div className="slide">
+            <h1>{data[8]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Ethereum: $2,000
+
+
+          <div className="slide">
+            <h1>{data[0]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Cardano: $1.50
+          <div className="slide">
+            <h1>{data[1]}</h1>
           </div>
-          <div className="horizontal-scrolling-items__item">
-            Binance Coin: $300
+          <div className="slide">
+            <h1>{data[2]}</h1>
           </div>
+          <div className="slide">
+            <h1>{data[3]}</h1>
+          </div>
+          <div className="slide">
+            <h1>{data[4]}</h1>
+          </div>
+          <div className="slide">
+            <h1>{data[5]}</h1>
+          </div>
+          <div className="slide">
+            <h1>{data[6]}</h1>
+          </div>
+          <div className="slide">
+            <h1>{data[7]}</h1>
+          </div>
+          <div className="slide">
+            <h1>{data[8]}</h1>
+          </div>
+
         </div>
       </div>
     </Container>
