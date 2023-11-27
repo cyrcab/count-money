08a0dedea203eed4399232837938824e7dce193c
ClementMendes <clement.mendes@epitech.eu>
2023-11-27 17:29:39 +0100

    :construction: websocket connexion

--- packages/front-end/src/Components/HeadBandCryptoPrice.tsx
+++ packages/front-end/src/Components/HeadBandCryptoPrice.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "@mui/material";
 import "../Css/HeadBandCryptoPrice.css";
 
@@ -7,76 +7,62 @@ const data = [
   "Ethereum: $2,000",
   "Cardano: $1.50",
   "Binance Coin: $300",
-  "ripple: $0.50",
+  "Ripple: $0.50",
   "Dogecoin: $0.10",
   "Tether: $1.00",
   "Polkadot: $40.00",
   "Bitcoin Cash: $500",
 ];
 
+interface CryptoData {
+  symbol: string;
+  price: string;
+}
+
 const HeadBandCryptoPrice: React.FC = () => {
+  const [cryptoData, setCryptoData] = useState<CryptoData | null>(null);
+
+  // List of cryptocurrency symbols
+  const cryptoSymbols = ["btcusdt", "ethusdt", "adausdt", "bnbusdt", "xrpusdt", "dogeusdt", "usdtusdt", "dotusdt", "bchusdt"];
+
+  useEffect(() => {
+    const ws = new WebSocket(`wss://stream.binance.com:9443/ws/${cryptoSymbols.join('@trade/') + '@trade'}`);
+
+    ws.onmessage = (event) => {
+      const eventData = JSON.parse(event.data);
+      const symbol = eventData.s;
+      const price = parseFloat(eventData.p).toFixed(2);
+
+      setCryptoData({ symbol, price });
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [cryptoSymbols]);
+
   return (
     <Container className="containerHeadBand" maxWidth="xl">
       <div className="slider">
         <div className="slide-track">
-          
-          <div className="slide">
-            <h1>{data[0]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[1]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[2]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[3]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[4]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[5]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[6]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[7]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[8]}</h1>
-          </div>
-
-
-          <div className="slide">
-            <h1>{data[0]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[1]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[2]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[3]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[4]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[5]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[6]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[7]}</h1>
-          </div>
-          <div className="slide">
-            <h1>{data[8]}</h1>
-          </div>
-
+          {cryptoData && (
+            cryptoSymbols.map((symbol, index) => (
+              <div key={index} className="slide">
+                <h1>
+                  {symbol.toUpperCase()}: €{cryptoData.price}
+                </h1>
+              </div>
+            ))
+          )}
+          {cryptoData && (
+            cryptoSymbols.map((symbol, index) => (
+              <div key={index} className="slide">
+                <h1>
+                  {symbol.toUpperCase()}: €{cryptoData.price}
+                </h1>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </Container>
