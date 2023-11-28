fa0fcabb436315e1d55d369fb754dae5578b2f15
ClementMendes <clement.mendes@epitech.eu>
2023-11-28 16:44:00 +0100

    :construction: mise en place de la config axios

---
+++ packages/front-end/src/axios.config.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+  // baseURL: 'http://localhost:4000', // Remplacez par l'URL de votre backend
+  timeout: 5000, // Définissez un délai d'attente (en millisecondes) si nécessaire
+  headers: {
+    'Content-Type': 'application/json', // Exemple d'en-tête personnalisé
+    // 'Authorization': 'Bearer ' + localStorage.getItem('token'), // En-tête d'autorisation (si nécessaire)
+  },
+});
+
+export default api;
