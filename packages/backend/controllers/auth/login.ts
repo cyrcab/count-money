e2f555b8c83e63e849e582266086f8d95d23cecf
ribcoco <ribcoco@gmail.com>
2023-12-01 16:09:11 +0100

    feat: 🎸 Login/test/seeder backend

---
+++ packages/backend/controllers/auth/login.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from "@prisma/client";
+import { User as UserInterface } from "@prisma/client";
+import { generateToken } from "./utils/Token";
+import {comparePwd} from './utils/Pwd';
+
+const prismaUser = new PrismaClient().user;
+
+export async function loginUser(data: Partial<UserInterface>) {
+    const result = {} as {
+        status: number;
+        body: { token: string; user: Partial<UserInterface> };
+    };
+
+    const { email, password } = data;
+
+    if (!email || !password) {
+        return { status: 400, body: { msg: "Please enter all fields" } };
+    }
+
+    // regex for email validation
+    const regexEmail =
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    // check if email is valid
+    if (!regexEmail.test(email)) {
+        return { status: 400, body: { msg: "Please enter a valid email" } };
+    }
+
+    try {
+        // check if user exists
+        const user = await prismaUser.findUnique({
+            where: {
+                email: email,
+            },
+        });
+        if (!user) {
+            return { status: 400, body: { msg: "Invalid credentials" } };
+        }
+
+        // check if password is correct
+        const isMatch = await comparePwd(password, user.password);
+        if (!isMatch) {
+            return { status: 400, body: { msg: "Invalid credentials" } };
+        }
+
+        const token = generateToken(user);
+        return {
+            status: 200,
+            body: {
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    firstname: user.firstname,
+                    lastname: user.lastname,
+                },
+            },
+        };
+    } catch (err) {
+        return { status: 500, body: { msg: err.message } };
+    }
+}
--- packages/backend/jest/auth.test.ts
+++ packages/backend/jest/auth.test.ts
@@ -1,5 +1,167 @@
 import { PrismaClient } from '@prisma/client';
 import { registerUser } from '../controllers/auth/register';
+import { loginUser } from '../controllers/auth/login';
+
+const prisma = new PrismaClient();
+
+beforeAll(async () => {
+
+    await prisma.$executeRaw`SET foreign_key_checks = 0;`;
+    
+    // Clear database before tests
+    await prisma.$executeRaw`TRUNCATE TABLE User;`;
+
+    await prisma.$executeRaw`SET foreign_key_checks = 1;`;
+});
+
+afterAll(async () => {
+
+    await prisma.$executeRaw`SET foreign_key_checks = 0;`;
+    
+    // Clear database before tests
+    await prisma.$executeRaw`TRUNCATE TABLE User;`;
+
+    await prisma.$executeRaw`SET foreign_key_checks = 1;`;
+    
+    // Disconnect from the database after all tests
+    await prisma.$disconnect();
+
+    // Close the server after all tests
+});
+
+describe('User Registration API', () => {
+    it('should register a new user', async () => {
+        const userData = {
+            email: 'test@test.com',
+            firstname: 'testF',
+            lastname: 'testL',
+            password: 'test',
+        };
+
+        const res = await registerUser(userData);
+
+        expect(res.status).toBe(201);
+        expect(res.body).toHaveProperty('token');
+        expect(res.body).toHaveProperty('user');
+        expect(res.body.user).toHaveProperty('id');
+        expect(res.body.user.email).toBe(userData.email);
+        expect(res.body.user.firstname).toBe(userData.firstname);
+        expect(res.body.user.lastname).toBe(userData.lastname);
+    });
+
+    it('should not register a new user if email already exists', async () => {
+        const userData = {
+            email: 'test@test.com',
+            firstname: 'testF',
+            lastname: 'testL',
+            password: 'test',
+        };
+
+        const res = await registerUser(userData);
+
+
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('msg');
+        expect(res.body.msg).toBe('Email already use');
+    });
+
+    it('should not register a new user if email is invalid', async () => {
+        const userData = {
+            email: 'test',
+            firstname: 'testF',
+            lastname: 'testL',
+            password: 'test',
+        };
+
+        const res = await registerUser(userData);
+
+
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('msg');
+        expect(res.body.msg).toBe('Please enter a valid email');
+    });
+
+    it('should not register a new user if one field is empty', async () => {
+        const userData = {
+            email: 'test2@test.com',
+            password: 'test',
+        };
+
+        const res = await registerUser(userData);
+
+
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('msg');
+        expect(res.body.msg).toBe('Please enter all fields');
+    });
+
+    it('should not register a new user if all fields are empty', async () => {
+        const res = await registerUser({});
+
+
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('msg');
+        expect(res.body.msg).toBe('Please enter all fields');
+    });
+});
+
+// Check login
+describe('User login', () => {
+    it('should login a user', async () => {
+        const userData = {
+            email: 'test@test.com',
+            password: 'test',
+        };
+
+        const res = await loginUser(userData);
+
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('token');
+        expect(res.body).toHaveProperty('user');
+        expect(res.body.user).toHaveProperty('id');
+        expect(res.body.user.email).toBe(userData.email);
+    });
+
+    it('should not login a user if email is invalid', async () => {
+        const userData = {
+            email: 'test',
+            password: 'test',
+        };
+
+        const res = await loginUser(userData);
+
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('msg');
+        expect(res.body.msg).toBe('Please enter a valid email');
+    });
+
+    it('should not login a user if email is not found', async () => {
+        const userData = {
+            email: 'test2@gmail.com',
+            password: 'test',
+        };
+
+        const res = await loginUser(userData);
+        
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('msg');
+        expect(res.body.msg).toBe('Invalid credentials');
+    });
+
+    it('should not login a user if password is incorrect', async () => {
+        const userData = {
+            email: 'test@test.com',
+            password: 'test2',
+        };
+
+        const res = await loginUser(userData);
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('msg');
+        expect(res.body.msg).toBe('Invalid credentials');
+    });
+
+})
+
 
 const prisma = new PrismaClient();
 // const app = express();
