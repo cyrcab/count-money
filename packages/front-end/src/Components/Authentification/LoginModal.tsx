082b403680c2756751e01db811cbbf933f2f994f
ClementMendes <clement.mendes@epitech.eu>
2023-12-04 10:35:23 +0100

    :white_check_mark: logique bouton signup/login

--- packages/front-end/src/Components/Authentification/LoginModal.tsx
+++ packages/front-end/src/Components/Authentification/LoginModal.tsx
@@ -16,16 +16,26 @@ const style = {
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-  textAlign: "center", // Ajout de la propriété textAlign pour l'alignement central
+  textAlign: "center",
 };
 
 interface LoginModalProps {
   onClose: () => void;
+  actionType: "Login" | "Sign Up";
+  onLoginSuccess: () => void;
+  onSwitchAction: () => void; // Ajout de la nouvelle prop pour basculer entre Login et Sign Up
 }
 
-const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
+const LoginModal: React.FC<LoginModalProps> = ({
+  onClose,
+  actionType,
+  onLoginSuccess,
+  onSwitchAction,
+}) => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -35,10 +45,32 @@ const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
     setPassword(event.target.value);
   };
 
+  const handleFirstNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(event.target.value);
+  };
+
   const handleLogin = () => {
-    // Ajoutez ici la logique de connexion avec l'email et le mot de passe
+    console.log("Action Type:", actionType);
     console.log("Email:", email);
     console.log("Password:", password);
+
+    if (actionType === "Sign Up") {
+      console.log("First Name:", firstName);
+      console.log("Last Name:", lastName);
+    }
+
+    const loginSuccessful = true;
+
+    if (loginSuccessful) {
+      onLoginSuccess();
+    }
+
     onClose();
   };
 
@@ -51,8 +83,28 @@ const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
     >
       <Box sx={style}>
         <Typography id="modal-modal-title" variant="h6" component="h2">
-          Login
+          {actionType}
         </Typography>
+        {actionType === "Sign Up" && (
+          <>
+            <TextField
+              label="First Name"
+              type="text"
+              value={firstName}
+              onChange={handleFirstNameChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Last Name"
+              type="text"
+              value={lastName}
+              onChange={handleLastNameChange}
+              fullWidth
+              margin="normal"
+            />
+          </>
+        )}
         <TextField
           label="Email"
           type="email"
@@ -69,14 +121,32 @@ const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
           fullWidth
           margin="normal"
         />
-        <Button style={{width:'80%', margin:10}} variant="contained" color="primary" onClick={handleLogin}>
-          Login
+        <Button
+          style={{ width: "80%", margin: 10 }}
+          variant="contained"
+          color="primary"
+          onClick={handleLogin}
+        >
+          {actionType}
         </Button>
-        <Divider >ou</Divider>
-        <Button style={{width:'80%', margin:10}} variant="contained" color="primary" onClick={handleLogin}>
-         <img src="" alt="" />
+        <Divider>ou</Divider>
+        <Button
+          style={{ width: "80%", margin: 10 }}
+          variant="contained"
+          color="primary"
+          onClick={handleLogin}
+        >
+          <img src="" alt="" />
           Google
         </Button>
+        <Typography
+          style={{ cursor: "pointer", color: "blue", marginTop: 10 }}
+          onClick={() => onSwitchAction()} // Appeler la fonction pour basculer entre Login et Sign Up
+        >
+          {actionType === "Sign Up"
+            ? "Si vous avez déjà un compte, connectez-vous."
+            : "Si vous n'avez pas de compte, inscrivez-vous."}
+        </Typography>
       </Box>
     </Modal>
   );
