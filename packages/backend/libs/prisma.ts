4210b1cabf314589d8e618fa89082ddf416afbc2
cyril cabrolier <cyr.cab@hotmail.fr>
2023-12-05 12:47:37 +0100

    fix: 🐛 bug on test and update github ci action for mysql

--- packages/backend/libs/prisma.ts
+++ packages/backend/libs/prisma.ts
@@ -1,3 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+  datasourceUrl:
+    process.env.NODE_ENV === 'test ' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+})
