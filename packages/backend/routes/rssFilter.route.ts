04b5a9585730dc43f6418b76cef039cd6daa9dd8
ribcoco <101266434+ribcoco@users.noreply.github.com>
2023-12-11 16:53:22 +0100

    EPI-21 feat: delete rss filter

--- packages/backend/routes/rssFilter.route.ts
+++ packages/backend/routes/rssFilter.route.ts
@@ -1,13 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const router = require('express').Router()
 
-import {addRssFilter, getRssFilterController, getRssFiltersController, updateRssFilterController } from "../controllers/rss_filter/rssFilter.controller";
+import {addRssFilter, getRssFilterController, getRssFiltersController, updateRssFilterController, deleteRssFilterController } from "../controllers/rss_filter/rssFilter.controller";
 import { verifyAdmin, verifyUser } from "../middleware/token.middleware";
 
 router.post('/',verifyAdmin, addRssFilter)
 router.get('/:id',verifyAdmin, getRssFilterController)
 router.get('/',verifyUser, getRssFiltersController)
 router.patch('/:id',verifyAdmin, updateRssFilterController)
+router.delete('/:id',verifyAdmin, deleteRssFilterController)
 
 
 export default router
