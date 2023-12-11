5b5dbd86d9445cf58f45fd041cce626389c69994
ribcoco <101266434+ribcoco@users.noreply.github.com>
2023-12-11 16:12:15 +0100

    EPI-21 feat: get one rss filter

--- packages/backend/controllers/rss_filter/rssFilter.controller.ts
+++ packages/backend/controllers/rss_filter/rssFilter.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
 import {addNewRssFilter} from './addRssFilter'
+import {getRssFilter, ParamRequest} from './getRssFilter'
 
 export async function addRssFilter(req: Request, res: Response) {
     const result = await addNewRssFilter(req.body)
+    return res.status(result.status).json(result.body);
+}
+
+export async function getRssFilterController(req: ParamRequest, res: Response) {
+    const result = await getRssFilter(req)
     return res.status(result.status).json(result.body);
 }
