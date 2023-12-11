c4bacf602fad55f29e9bc0f4a3ecbafe4c7e8c4a
ribcoco <101266434+ribcoco@users.noreply.github.com>
2023-12-11 16:41:25 +0100

    EPI-21 feat: update rss filter controller

--- packages/backend/controllers/rss_filter/rssFilter.controller.ts
+++ packages/backend/controllers/rss_filter/rssFilter.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import {addNewRssFilter} from './addRssFilter'
 import {getRssFilter, ParamRequest, getRssFilters} from './getRssFilter'
+import { updateRss } from "./updateRssFilter";
 
 export async function addRssFilter(req: Request, res: Response) {
     const result = await addNewRssFilter(req.body)
@@ -14,5 +15,12 @@ export async function getRssFilterController(req: ParamRequest, res: Response) {
 
 export async function getRssFiltersController(req: Request, res: Response) {
     const result = await getRssFilters()
+    return res.status(result.status).json(result.body);
+}
+
+export async function updateRssFilterController(req: Request, res: Response) {
+    const id = req.params.id
+
+    const result = await updateRss(req.body, parseInt(id,10))
     return res.status(result.status).json(result.body);
 }
