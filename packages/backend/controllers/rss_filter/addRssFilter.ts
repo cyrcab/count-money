da8d36bb2eb556e3402d32a7b92d485e06196240
ribcoco <101266434+ribcoco@users.noreply.github.com>
2023-12-12 10:13:22 +0100

    EPI-21 feat: add rss filter to user

--- packages/backend/controllers/rss_filter/addRssFilter.ts
+++ packages/backend/controllers/rss_filter/addRssFilter.ts
@@ -2,6 +2,8 @@ import {prisma} from '../../libs/prisma'
 import { RSS_filter as RssFilterInterface } from '@prisma/client'
 
 const prismaRssFilter = prisma.rSS_filter
+const prismaUser = prisma.user
+const prismaRssFilterUser = prisma.userRSS_filter
 
 export async function addNewRssFilter(data: Partial<RssFilterInterface>) {
 
@@ -19,6 +21,59 @@ export async function addNewRssFilter(data: Partial<RssFilterInterface>) {
             },
         })
         return { status: 201, body: { rssFilter } }
+    } catch (error) {
+        return { status: 500, body: { msg: error.message } }
+    }
+}
+
+export async function addRssToUser(idUser: number, idRss: number) {
+
+    if (!idUser || !idRss ) {
+        return { status: 400, body: { msg: 'Please enter all fields' } }
+    }
+    
+    try {
+        const user = await prismaUser.findUnique({
+            where: {
+                id: idUser,
+            },
+        })
+
+        if(!user) {
+            return { status: 400, body: { msg: 'No user found' } }
+        }
+
+        const rssFilter = await prismaRssFilter.findUnique({
+            where: {
+                id: idRss,
+            },
+        })
+
+        if(!rssFilter) {
+            return { status: 400, body: { msg: 'No RSS filter found' } }
+        }
+
+        const userRssFilter = await prismaRssFilterUser.findUnique({
+            where: {
+                userId_RSS_filterId: {
+                  userId: idUser,
+                  RSS_filterId: idRss,
+                },
+            },
+        })
+
+        if(userRssFilter) {
+            return { status: 400, body: { msg: 'RSS filter already added' } }
+        }
+
+        await prismaRssFilterUser.create({
+            data: {
+                userId: idUser,
+                RSS_filterId: idRss,
+            },
+        })
+
+        return { status: 201, body: { msg: 'RSS filter added' } }
     } catch (error) {
         return { status: 500, body: { msg: error.message } }
     }
--- packages/backend/routes/rssFilter.route.ts
+++ packages/backend/routes/rssFilter.route.ts
@@ -1,10 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const router = require('express').Router()
 
-import {addRssFilter, getRssFilterController, getRssFiltersController, updateRssFilterController, deleteRssFilterController } from "../controllers/rss_filter/rssFilter.controller";
-import { verifyAdmin, verifyUser } from "../middleware/token.middleware";
+import {addRssFilter, getRssFilterController, getRssFiltersController, updateRssFilterController, deleteRssFilterController, addRssToUserController } from "../controllers/rss_filter/rssFilter.controller";
+import { verifyAdmin, verifyUser, verifyAdminOrMe } from "../middleware/token.middleware";
 
 router.post('/',verifyAdmin, addRssFilter)
+router.post('/:id/:idRss',verifyAdminOrMe, addRssToUserController)
 router.get('/:id',verifyAdmin, getRssFilterController)
 router.get('/',verifyUser, getRssFiltersController)
 router.patch('/:id',verifyAdmin, updateRssFilterController)
