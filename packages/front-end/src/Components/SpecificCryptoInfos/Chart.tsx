0a402938ec41479030a68426bff834a6071362f9
ClementMendes <clement.mendes@epitech.eu>
2023-12-12 11:41:48 +0100

    :construction: chart on crypto

--- packages/front-end/src/Components/SpecificCryptoInfos/Chart.tsx
+++ packages/front-end/src/Components/SpecificCryptoInfos/Chart.tsx
@@ -1,38 +1,73 @@
-// ChartComponent.tsx
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-interface ChartComponentProps {
-  // Ajoutez les propriétés nécessaires pour le composant de graphique
-}
+import ReactApexChart from 'react-apexcharts';
+import "../../Css/chart.css"
+
+interface ChartComponentProps {}
 
 const ChartComponent: React.FC<ChartComponentProps> = () => {
-  // Implémentez ici la logique pour le composant de graphique
+  const [chartOptions, setChartOptions] = useState({
+    chart: {
+      id: 'basic-line',
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    },
+  });
+
+  const [series, setSeries] = useState([
+    {
+      name: 'Series 1',
+      data: [30, 40, 25, 50, 49, 21, 70, 51, 60, 45, 30, 20],
+    },
+  ]);
+
+  const generateRandomData = () => {
+    const newData = Array.from({ length: 12 }, () => Math.floor(Math.random() * 100));
+    return newData;
+  };
+
+  const handleTimeRangeChange = (range: string) => {
+    // Générez de nouvelles données aléatoires
+    const newData = generateRandomData();
+
+    // Mettez à jour les données du graphique avec les nouvelles données
+    setSeries([{ name: 'Series 1', data: newData }]);
+
+    console.log(`Changing time range to ${range}`);
+    // Mettez à jour les options du graphique en fonction de la plage de temps sélectionnée
+    // (ex. charger des données différentes en fonction de la plage de temps)
+  };
+
   return (
-    <Card sx={{ minWidth: 275 }}>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        Charts
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
+    <Card className='containerChart'>
+      <CardContent>
+        <ReactApexChart
+          options={chartOptions}
+          series={series}
+          type="line"
+          height={180}
+        />
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={() => handleTimeRangeChange('year')}>
+          Année
+        </Button>
+        <Button size="small" onClick={() => handleTimeRangeChange('month')}>
+          Mois
+        </Button>
+        <Button size="small" onClick={() => handleTimeRangeChange('week')}>
+          Semaine
+        </Button>
+        <Button size="small" onClick={() => handleTimeRangeChange('day')}>
+          Jour
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
