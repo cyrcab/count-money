c86fd6893657a8443b9f034e328bc2e776343710
ribcoco <101266434+ribcoco@users.noreply.github.com>
2023-12-12 11:13:59 +0100

    EPI-21 feat: delete user filter rss

--- packages/backend/controllers/rss_filter/deleteRssFilter.ts
+++ packages/backend/controllers/rss_filter/deleteRssFilter.ts
@@ -1,6 +1,8 @@
 import {prisma} from '../../libs/prisma'
 
 const prismaRssFilter = prisma.rSS_filter
+const prismaUser = prisma.user
+const prismaRssFilterUser = prisma.userRSS_filter
 
 export async function deleteRss(id: number) {
             
@@ -30,4 +32,60 @@ export async function deleteRss(id: number) {
     } catch (error) {
         return { status: 500, body: { msg: error.message } }
     }
+}
+
+export async function deleteRssToUser(idUser: number, idRss: number) {
+    
+        if (!idUser || !idRss ) {
+            return { status: 400, body: { msg: 'Please enter all fields' } }
+        }
+        
+        try {
+            const user = await prismaUser.findUnique({
+                where: {
+                    id: idUser,
+                },
+            })
+    
+            if(!user) {
+                return { status: 400, body: { msg: 'No user found' } }
+            }
+    
+            const rssFilter = await prismaRssFilter.findUnique({
+                where: {
+                    id: idRss,
+                },
+            })
+    
+            if(!rssFilter) {
+                return { status: 400, body: { msg: 'No RSS filter found' } }
+            }
+    
+            const userRssFilter = await prismaRssFilterUser.findUnique({
+                where: {
+                    userId_RSS_filterId: {
+                    userId: idUser,
+                    RSS_filterId: idRss,
+                    },
+                },
+            })
+    
+            if(!userRssFilter) {
+                return { status: 400, body: { msg: 'No RSS filter found' } }
+            }
+    
+            await prismaRssFilterUser.delete({
+                where: {
+                    userId_RSS_filterId: {
+                    userId: idUser,
+                    RSS_filterId: idRss,
+                    },
+                },
+            })
+    
+            return { status: 200, body: { userRssFilter }}
+            
+        } catch (error) {
+            return { status: 500, body: { msg: error.message } }
+        }
 }
