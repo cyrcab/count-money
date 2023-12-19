ccaef1e07273b7949c5d3bdf9364b0eaffed9b52
AlexMerigot <104435310+AlexMerigot@users.noreply.github.com>
2023-12-19 11:11:41 +0100

    Page Admin OK

--- packages/front-end/src/Components/admin/RssManagement.tsx
+++ packages/front-end/src/Components/admin/RssManagement.tsx
@@ -1,83 +1,211 @@
-import { useEffect, useState } from "react";
-import "../../Css/RssManagement.css";
+import React, { useState, useEffect } from "react";
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form";
 import api from "../../axios.config";
-import {
-  Button,
-  Container,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Typography,
-} from "@mui/material";
-import { SelectChangeEvent } from "@mui/material/Select";
+import "../../Css/RssManagement.css";
 
 interface RssFilter {
   id: number;
   name: string;
+  url: string;
 }
 
 const initialFilter: RssFilter = {
   id: 0,
   name: "",
+  url: "",
 };
 
 const RssManagement: React.FC = () => {
   const [rssFilters, setRssFilters] = useState<RssFilter[]>([]);
+  const [isEditPopupOpen, setEditPopupOpen] = useState(false);
+  const [isAddPopupOpen, setAddPopupOpen] = useState(false);
   const [selectedFilter, setSelectedFilter] = useState<RssFilter>(initialFilter);
+  const [isDeletePopupOpen, setDeletePopupOpen] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+  } = useForm<RssFilter>();
 
   useEffect(() => {
     api.get("/rss_filter/")
       .then((response) => {
-        setRssFilters(response.data.body.rss_filter || []);
+        const responseData = response.data.rssFilters;
+        const fetchedFilters = responseData ? responseData || [] : [];
+        setRssFilters(fetchedFilters);
       })
       .catch((error) => {
-        console.error("Erreur lors de la récupération des utilisateurs :", error);
+        console.error("Erreur lors de la récupération des filtres RSS :", error);
       });
   }, []);
 
-  const handleSelectChange = (event: SelectChangeEvent<number>) => {
-    const selectedId = event.target.value as number;
-    const selected = rssFilters.find((filter) => filter.id === selectedId);
-    setSelectedFilter(selected || initialFilter);
+  const openEditPopup = (filter: RssFilter) => {
+    setSelectedFilter(filter);
+    setEditPopupOpen(true);
+  };
+
+  const closeEditPopup = () => {
+    setEditPopupOpen(false);
+  };
+
+  const openAddPopup = () => {
+    setAddPopupOpen(true);
+  };
+
+  const closeAddPopup = () => {
+    setAddPopupOpen(false);
+  };
+
+  const editFilter: SubmitHandler<RssFilter> = (data) => {
+    const updatedFilters = rssFilters.map((filter) =>
+      filter.id === selectedFilter.id ? data : filter
+    );
+    setRssFilters(updatedFilters);
+    closeEditPopup();
+    const dataToSend = {
+      name: data.name,
+      url: data.url,
+    };
+    api.patch(`/rss_filter/${selectedFilter.id.toString()}`, dataToSend)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la modification du filtre RSS :", error);
+      });
+  };
+
+  const openDeletePopup = (filter: RssFilter) => {
+    setSelectedFilter(filter);
+    setDeletePopupOpen(true);
+  };
+
+  const closeDeletePopup = () => {
+    setDeletePopupOpen(false);
+  };
+
+  const addFilter: SubmitHandler<RssFilter> = (data) => {
+    const updatedFilters = [...rssFilters, data];
+    setRssFilters(updatedFilters);
+    closeAddPopup();
+    const dataToSend = {
+      name: data.name,
+      url: data.url,
+    };
+    api.post("/rss_filter/", dataToSend)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de l'ajout du filtre RSS :", error);
+      });
+  };
+
+  const deleteRssFilter = () => {
+    const updatedFilters = rssFilters.filter(
+      (filter) => filter.name !== selectedFilter.name
+    );
+    setRssFilters(updatedFilters);
+    closeDeletePopup();
+    api.delete(`/rss_filter/${selectedFilter.id}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la suppression du filtre RSS :", error);
+      });
   };
 
   return (
-    <Container className="containerUserRssManagement">
-      <Typography variant="h2">Gestion des Flux Rss</Typography>
-      <br />
-      <Typography variant="h6">Flux Rss disponibles :</Typography>
-      <br />
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-helper-label">Flux Rss</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={selectedFilter.id || ""}
-          label="Flux Rss"
-          onChange={handleSelectChange}
-        >
-          <MenuItem value="" disabled>
-            Sélectionnez un Flux Rss
-          </MenuItem>
-          {rssFilters.map((rssFilter) => (
-            <MenuItem key={rssFilter.id} value={rssFilter.id}>
-              {rssFilter.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Typography variant="h6">Flux Rss sélectionné :</Typography>
-      {selectedFilter.id !== 0 && (
-        <Button
-          variant="contained"
-          onClick={() => setSelectedFilter(initialFilter)}
-          sx={{ margin: 1 }}
-        >
-          {selectedFilter.name} &#10006;
-        </Button>
-      )}
-    </Container>
+    <div>
+      <div className="containerRssManagement">
+        <Button onClick={openAddPopup}>Ajouter un filtre RSS</Button>
+        <TableContainer component={Paper} className="RssTableContainer">
+          <Table className="RssTable">
+            <TableHead>
+              <TableRow>
+                <TableCell>Nom</TableCell>
+                <TableCell>URL</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rssFilters.map((filter, index) => (
+                <TableRow key={index}>
+                  <TableCell>{filter.name}</TableCell>
+                  <TableCell>{filter.url}</TableCell>
+                  <TableCell>
+                    <Button onClick={() => openEditPopup(filter)}>Éditer</Button>
+                    <Button onClick={() => openDeletePopup(filter)}>Supprimer</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        {/* Edit Popup */}
+        <Dialog open={isEditPopupOpen} onClose={closeEditPopup}>
+          <DialogTitle>Éditer un filtre RSS</DialogTitle>
+          <DialogContent>
+            <form onSubmit={handleSubmit(editFilter)} className="custom-dialog">
+              <TextField
+                label="Nom"
+                defaultValue={selectedFilter.name}
+                {...register("name")}
+              />
+              <TextField
+                label="URL"
+                defaultValue={selectedFilter.url}
+                {...register("url")}
+              />
+              <div className="DialogButton">
+                <Button type="submit">Éditer</Button>
+                <DialogActions>
+                  <Button onClick={closeEditPopup}>Fermer</Button>
+                </DialogActions>
+              </div>
+            </form>
+          </DialogContent>
+        </Dialog>
+
+        {/* Add Popup */}
+        <Dialog open={isAddPopupOpen} onClose={closeAddPopup}>
+          <DialogTitle>Ajouter un filtre RSS</DialogTitle>
+          <DialogContent>
+            <form onSubmit={handleSubmit(addFilter)} className="custom-dialog">
+              <TextField
+                label="Nom"
+                {...register("name", { required: true })}
+              />
+              <TextField
+                label="URL"
+                {...register("url", { required: true })}
+              />
+              <div className="DialogButton">
+                <Button type="submit">Ajouter</Button>
+                <DialogActions>
+                  <Button onClick={closeAddPopup}>Fermer</Button>
+                </DialogActions>
+              </div>
+            </form>
+          </DialogContent>
+        </Dialog>
+
+        <Dialog open={isDeletePopupOpen} onClose={closeDeletePopup}>
+          <DialogTitle>Supprimer un Flux Rss</DialogTitle>
+          <DialogContent>
+            Êtes-vous sûr de vouloir supprimer l'utilisateur{" "}
+            <strong>{selectedFilter.name}</strong> ?
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={deleteRssFilter}>Supprimer</Button>
+            <Button onClick={closeDeletePopup}>Annuler</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    </div>
   );
 };
 
--- packages/front-end/src/axios.config.ts
+++ packages/front-end/src/axios.config.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   timeout: 5000, // Définissez un délai d'attente (en millisecondes) si nécessaire
   headers: {
     'Content-Type': 'application/json', // Exemple d'en-tête personnalisé
-    'Authorization': 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJyaWJjb2NvMkBnbWFpbC5jb20iLCJyb2xlIjoxLCJpYXQiOjE3MDI5MDk0NTksImV4cCI6MTcwMjk5NTg1OX0.lfEA3Nz9LcaQHUMqhXZmKI1LFalxYisMPtQEmfWiwAI", // En-tête d'autorisation (si nécessaire)
+    'Authorization': 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJyaWJjb2NvMkBnbWFpbC5jb20iLCJyb2xlIjoxLCJpYXQiOjE3MDI5MTE5NTAsImV4cCI6MTcwMjk5ODM1MH0.ViCYcAd-lTzGf7nLawdRECuu2r3IsLqw38IyCkoe6yY", // En-tête d'autorisation (si nécessaire)
   },
 });
 
