13725abc48d2ef4c4ceb2f098b2dd1d159e82196
AlexMerigot <104435310+AlexMerigot@users.noreply.github.com>
2024-01-06 17:02:30 +0100

    ajout des routes pour les données users

--- packages/front-end/src/Components/Profile/UserInfo.tsx
+++ packages/front-end/src/Components/Profile/UserInfo.tsx
@@ -1,35 +1,77 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Typography, Button, TextField, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
 import "../../Css/UserInfo.css";
+import api from "../../axios.config";
 
 const UserInfo: React.FC = () => {
   const [openDialog, setOpenDialog] = useState(false);
+  const [userData, setUserData] = useState({
+    id: 0,
+    username: "",
+    email: "",
+    lastName: "",
+    firstName: "",
+  });
 
   const handleEditClick = () => {
     setOpenDialog(true);
   };
 
   const handleSaveClick = () => {
-    // Add logic to save the updated information
-    setOpenDialog(false);
+    const updatedUserData = {
+      id: userData.id,
+      username: "Nouveau Pseudo", 
+      email: "Nouveau Email", 
+      lastName: "Nouveau nom", 
+      firstName: "Nouveau Prenom", 
+    };
+
+    api.put(`/user/${userData.id}`, updatedUserData)
+      .then((response) => {
+        console.log(response);
+        setUserData(updatedUserData);
+        setOpenDialog(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
 
   const handleCloseDialog = () => {
     setOpenDialog(false);
   };
+
+  useEffect(() => {
+    api.get("/user/me")
+    .then((response) => {
+      const responseData = response.data.body.user;
+      setUserData({
+        id: responseData.id,
+        username: responseData.username,
+        email: responseData.email,
+        lastName: responseData.lastName,
+        firstName: responseData.firstName,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+  , []);
 
   return (
     <Container className="containerUserInfo">
       <Typography variant="h3">Informations Profil</Typography>
       <br/>
       <div className="infoBox">
-        <Typography variant="h4">Pseudo : JohnDoe</Typography>
+        <Typography variant="h4">Pseudo : {userData.username}</Typography>
         <br/>
-        <Typography variant="h4">Email : john.doe@example.com</Typography>
+        <Typography variant="h4">Email : {userData.email}</Typography>
         <br/>
-        <Typography variant="h4">Nom : Doe</Typography>
+        <Typography variant="h4">Nom : {userData.lastName}</Typography>
         <br/>
-        <Typography variant="h4">Prénom : John</Typography>
+        <Typography variant="h4">Prénom : {userData.firstName}</Typography>
         <br/>
         <Button variant="contained" onClick={handleEditClick}>
           Modifier
@@ -37,10 +79,10 @@ const UserInfo: React.FC = () => {
         <Dialog open={openDialog} className="dialogProfil" onClose={handleCloseDialog}>
           <DialogTitle>Modifier les informations</DialogTitle>
           <DialogContent className="dialogProfilEdit">
-            <TextField label="Nouveau Pseudo" variant="outlined" fullWidth />
-            <TextField label="Nouveau Email" variant="outlined" fullWidth />
-            <TextField label="Nouveau nom" variant="outlined" fullWidth />
-            <TextField label="Nouveau Prenom" variant="outlined" fullWidth />
+            <TextField label={userData.username} variant="outlined" fullWidth />
+            <TextField label={userData.email} variant="outlined" fullWidth />
+            <TextField label={userData.lastName} variant="outlined" fullWidth />
+            <TextField label={userData.firstName} variant="outlined" fullWidth />
           </DialogContent>
           <DialogActions>
             <Button onClick={handleCloseDialog}>Annuler</Button>
@@ -53,5 +95,7 @@ const UserInfo: React.FC = () => {
     </Container>
   );
 };
+
+
 
 export default UserInfo;
