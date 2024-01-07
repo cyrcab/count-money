1373432bb5c6943ca5ccc58ca7186c31c6922b7f
ClementMendes <clement.mendes@epitech.eu>
2024-01-07 17:36:36 +0100

    :tada: récupération des articles

--- packages/front-end/src/Components/TopNewsInfos.tsx
+++ packages/front-end/src/Components/TopNewsInfos.tsx
@@ -1,11 +1,48 @@
 import "../Css/TopNewsInfos.css";
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
+import defaultImage from "../assets/article.jpg";
 
-const TopNewsInfos: React.FC = () => {
+interface TopNewsInfosProps {
+  selectedArticles: {
+    imgSrc: string;
+    title: string;
+    link: string;
+  } | null;
+}
+
+const TopNewsInfos: React.FC<TopNewsInfosProps> = ({ selectedArticles }) => {
+  if (!selectedArticles) {
+    return (
+      <Container className="containerTopNewsInfos">
+        <div className="newsContainer">
+          <Typography variant="h2">Actualités</Typography>
+          <Typography variant="body1">Aucun article sélectionné.</Typography>
+        </div>
+      </Container>
+    );
+  }
+
   return (
     <Container className="containerTopNewsInfos">
       <div className="newsContainer">
-        <Typography variant="h2">Actualités</Typography>
+        <Typography variant="h2">{selectedArticles.title}</Typography>
+        <Button
+          variant="contained"
+          component="a"
+          href={selectedArticles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="buttonReadMore"
+        >
+          Lire l'article complet
+        </Button>
+        <img className="imgArticle"
+          src={
+            selectedArticles.imgSrc === "No image"
+              ? defaultImage
+              : selectedArticles.imgSrc
+          }
+        />
       </div>
     </Container>
   );
